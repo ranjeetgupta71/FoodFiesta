26a43f4c7ef9bc2a7687ee524fbdcc22271ea0b4
Ranjeet Kumar <ranjeet@pearlthoughts.com>
2023-09-27 12:44:55 +0530

    Adds flash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @message = "Zomato Clone"
+    flash[:notice] = "Logged in Successfully"
+    flash[:alert] = "Invalid email or password"
   end
 end
