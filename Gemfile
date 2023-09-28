0a4bced71e05398f584e99374fff82ba36dd39c1
Ranjeet Kumar <ranjeet@pearlthoughts.com>
2023-09-29 01:55:10 +0530

    user created on signup successfully

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+gem 'pry', '~> 0.14.1'
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,20 @@
-class RegistrationsController<ApplicationController
+class RegistrationsController < ApplicationController
   def new 
     @user = User.new
   end
 
   def create
-    flash[:success] = "Thanks for signing up!"
-    # flash[:notice] = "Thanks for signing up!"
-    redirect_to root_path
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
