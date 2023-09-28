a500a88e5a6c6472171a4be63539ed4b47557597
Ranjeet Kumar <ranjeet@pearlthoughts.com>
2023-09-29 04:48:39 +0530

    signin,session storage added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @message = "Zomato Clone"
-    flash[:notice] = "Logged in Successfully"
-    flash[:alert] = "Invalid email or password"
+    @user = User.find_by(id: session[:current_user_id])
   end
 end
--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -12,14 +12,9 @@ class SigninController < ApplicationController
     user = User.find_by(email: params[:user][:email])
 
     if user && user.authenticate(params[:user][:password])
-      # binding.pry
-      session[:user_id] = user.id
       session[:current_user_id] = user.id
-      # Successful sign-in
-      # signin[:user_email] = user.email
       redirect_to root_path
     else
-      # Failed sign-in
       flash.now[:alert] = 'Invalid email or password'
       render :new
     end
